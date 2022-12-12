cefeaa03b89a1f35cf3b739cf8d54e189b255f98
miranda0111 <gecyeung@gmail.com>
2022-12-12 16:35:01 +0800

    Update RT.js

--- RT.js
+++ RT.js
@@ -1,6 +1,6 @@
-const axios = require('axios');
-const FormData = require('form-data');
-const request = require('request');
+// const axios = require('axios');
+// const FormData = require('form-data');
+// const request = require('request');
 
 const $ = new Env('Rtool多人版');
 let waittime = 5000 //等待报名时间5000ms
@@ -296,42 +296,42 @@ function checkContains(msg, data){
     return index
 }
 
-async function uploadimg() {
-    return new Promise((resolve) => {
-    let data = new FormData();
-    data.append('biz_id', '6396bda5fa845a4ba7e48e77');
-    data.append('file', $request('https://www.baidu.com/img/bd_logo1.png'));
+// async function uploadimg() {
+//     return new Promise((resolve) => {
+//     let data = new FormData();
+//     data.append('biz_id', '6396bda5fa845a4ba7e48e77');
+//     data.append('file', request('https://www.baidu.com/img/bd_logo1.png'));
 
-    let config = {
-        method: 'post',
-        url: 'https://api-xcx-qunsou.weiyoubot.cn/xcx/image/v2/upload',
-        headers: { 
-          'Host': 'api-xcx-qunsou.weiyoubot.cn', 
-          'Accept-Encoding': 'gzip,compress,br,deflate', 
-          'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.29(0x18001d38) NetType/WIFI Language/zh_CN', 
-          'Content-Type': 'multipart/form-data; boundary=WABoundary+A405E0FFAB7D144AWA', 
-          'Referer': 'https://servicewechat.com/wxfaa08012777a431e/871/page-frame.html', 
-          'Cookie': 'tgw_l7_route=05fc80284c7b2840a1683128310f0d09', 
-          ...data.getHeaders()
-        },
-        data : data
-    };
-    axios(config)
-    .then(function (response) {
-        // data = JSON.parse(data)
-        // console.log(JSON.stringify(response.data));
-        let msgdata = response.data
-        if (msgdata.msg == "OK"){
-            msg_origin_field_value = msgdata.data.url
-            console.log("\n上传图片成功: " + msg_origin_field_value)
-        } 
+//     let config = {
+//         method: 'post',
+//         url: 'https://api-xcx-qunsou.weiyoubot.cn/xcx/image/v2/upload',
+//         headers: { 
+//           'Host': 'api-xcx-qunsou.weiyoubot.cn', 
+//           'Accept-Encoding': 'gzip,compress,br,deflate', 
+//           'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.29(0x18001d38) NetType/WIFI Language/zh_CN', 
+//           'Content-Type': 'multipart/form-data; boundary=WABoundary+A405E0FFAB7D144AWA', 
+//           'Referer': 'https://servicewechat.com/wxfaa08012777a431e/871/page-frame.html', 
+//           'Cookie': 'tgw_l7_route=05fc80284c7b2840a1683128310f0d09', 
+//           ...data.getHeaders()
+//         },
+//         data : data
+//     };
+//     axios(config)
+//     .then(function (response) {
+//         // data = JSON.parse(data)
+//         // console.log(JSON.stringify(response.data));
+//         let msgdata = response.data
+//         if (msgdata.msg == "OK"){
+//             msg_origin_field_value = msgdata.data.url
+//             console.log("\n上传图片成功: " + msg_origin_field_value)
+//         } 
     
-    })
-    .catch(function (error) {
-        console.log(error);
-    }); 
-}) 
-}
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     }); 
+// }) 
+// }
 
 //版块
 function sendinfo(timeout = 0) {
@@ -396,8 +396,8 @@ function sendinfo(timeout = 0) {
             if(msg_type_text[i] == "单张图片"){
                 _data.info[i] = {
                     "field_name":msg_field_name[i],
-                    "field_value":msg_origin_field_value,
-                    "origin_field_value":msg_origin_field_value,
+                    "field_value":["https:\/\/cdn-xcx-qunsou.weiyoubot.cn\/xcx\/2022-12-12\/97222522e9db4d98b523cec994487cf7.jpg"],
+                    "origin_field_value":["https:\/\/cdn-xcx-qunsou.weiyoubot.cn\/xcx\/2022-12-12\/97222522e9db4d98b523cec994487cf7.jpg"],
                     "field_key":msg_field_key[i],
                     "ignore":0
                 }
