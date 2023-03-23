a6d4ade2e21d6ab23575880e52f68aea75f99829
miranda0111 <gecyeung@gmail.com>
2023-03-23 21:34:41 +0800

    更新了些

--- RT_tt.py
+++ RT_tt.py
@@ -1,26 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 10:35:03 2023
+
+@author: 很多
+"""
+
 import base64
 import datetime
 import json
-import sys
+import os
 import time
 from datetime import datetime
 from pyzbar.pyzbar import decode
 from PIL import Image
 import qrcode
 import requests
+import urllib.request
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 import _thread
 import threading
+import re
+import pandas as pd
+import random
 
-import cv2
-#读取csv文件参数信息  并存储
 UserInfo= {},
 ClassEid = []
 ClassList = {}
 InfoList=[]
-
-# 要是你知道eid和你的tocken，本程序自动开始抢
-# eid = ''
-# tocken = ''
+Send_index = 1
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
     "Accept": "*/*",
@@ -32,29 +39,15 @@ headers = {
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "cross-site",
 }
-import csv
-import openpyxl
-def  ReadInfo():
-    info={}
-    print("##############################参数设置模块###############################")
 
-    wb = openpyxl.load_workbook("./参数.xlsx")
-    sheetName=wb.sheetnames
-    ws=wb[sheetName[0]]
-    for row in ws.rows:
-        line=[c.value for c in row]
-        # print([c.value for c in row])
-        key = line[0]
-        info[key] = line[1]
-    # with open('./参数.csv', 'r',encoding="utf-8") as myFile:
-    #     lines = csv.reader(myFile,skipinitialspace=True)
-    #     for line in lines:
-    #         key = line[0]
-    #         info[key] = line[1]
-    # # print(json.dumps(info, indent=4, ensure_ascii=False, sort_keys=True))
-    print(info)
+def ReadInfo():
+    print("##############################参数设置模块###############################")
+    df = pd.read_excel('./参数.xlsx', usecols=['参数标题', '参数内容'])# 读取excel文件
+    info = df.set_index('参数标题')['参数内容'].to_dict()# 将两列数据转换成字典格式
+    # print(info)
     print("##############################参数读取完成###############################")
     return info
+
 def Geterweima():
     print('获取二维码')
     url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll_web/v1/pc_code'
@@ -90,7 +83,6 @@ def GetToken():
         # print(ret['msg'])
     # return ret['data']['access_token']
 
-
 def GetOftenInfo(token):
     print('获取token')
     url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll/v1/userinfo?access_token=%s'%token
@@ -123,12 +115,66 @@ def GetTime(token, eid):
     print('获取时间')
     url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll/v2/detail?access_token=%s&eid=%s&admin=0&from=detail&referer=' % (token, eid)
     ret = requests.get(url, headers=headers).json()
-    print(ret)
+    # print(ret)
     if ret["msg"] != "ok":
         return
     print("您的选择是：" + ClassList[eid] +" 报名时间：" + datetime.fromtimestamp(ret["data"]["start_time"]).strftime("%Y-%m-%d %H:%M:%S") + "\n" )
     return ret["data"]["start_time"]
- #获取相关参数
+
+#应对超贱情况
+def Reg_Exp(text):
+    # text = "这是一段中文文本，包含了一些中文字符。"
+    pattern = re.compile(r'[\u4e00-\u9fa5]+')
+    result = pattern.findall(text)
+    chinese_str = ''.join(result)
+    return chinese_str
+
+#图片上传,在线获取URL图片链接
+def upload_pic(eid):
+    filename = "upload_image.jpg"
+    if os.path.isfile('./'+filename):
+        print(f'{filename}文件存在')
+    else:
+        print(f'{filename}文件不存在,生产中') 
+        download_urls = [
+                "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fci.xiaohongshu.com%2F48641552-06c0-37ac-8f6a-1f5e8752c4c0%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fci.xiaohongshu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1682143629&t=3ab7869f9aedb2824c5d6c6e339b0f42",
+                "https://www.baidu.com/img/flexible/logo/pc/result@2.png",
+                "https://static.zhihu.com/heifetz/assets/apple-touch-icon-152.81060cab.png",
+                "https://i0.hdslb.com/bfs/archive/bcac17c22b1458a90a0a943ecf4e00ad9482ab84.png",
+                "http://www.iqiyipic.com/pcwimg/128-128-logo.png",
+                "https://y.qq.com/mediastyle/yqq/img/logo.png?max_age=2592000"
+            ]
+        download_url = random.choice(download_urls)
+        urllib.request.urlretrieve(download_url, filename)
+        
+    url = "https://api-xcx-qunsou.weiyoubot.cn/xcx/image/v2/upload"
+    headers = {
+            "Host":"api-xcx-qunsou.weiyoubot.cn",
+            "Connection":"keep-alive",
+            "Content-Length":"227022",
+            "Accept-Encoding":"gzip,compress,br,deflate",
+            "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.33(0x18002128) NetType/4G Language/zh_CN",
+            "Referer":"https//servicewechat.com/wxfaa08012777a431e/904/page-frame.html"
+        }
+    multipart_encoder=MultipartEncoder(
+            fields={
+                    'biz_id': eid,'file': (filename,open('./'+filename,'rb'),'image/jpeg')
+                },
+            boundary='----WebKitFormBoundaryJ2aGzfsg35YqeT7X')
+    headers["Content-Type"]=multipart_encoder.content_type
+    response = requests.request("POST", url, headers=headers, data=multipart_encoder)
+    ret = response.json()
+    if ret['msg'] == 'OK':
+        pic_url = response.json()['data']['url']
+        print(f'上传图片成功：{pic_url}')
+    else:
+        global Send_index
+        Send_index = 0
+        pic_url = None
+        print('上传失败')
+    return pic_url
+    
+#获取相关参数
 def get_optioninfo(eid,token):
     headers = {
             'Accept': '*/*',
@@ -147,19 +193,42 @@ def get_optioninfo(eid,token):
             'sec-ch-ua-platform': '"Windows"'
         }
     try:
-            response = requests.request("GET",'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll/v3/detail?eid={eid}&access_token={token}&admin=0&from=detail&referer='.format(eid = eid, token = token), headers=headers,timeout=2)
+            response = requests.request("GET",'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll/v1/req_detail?eid={eid}&access_token={token}&admin=0&from=detail&referer='.format(eid = eid, token = token), headers=headers,timeout=2)
             data = response.json()
             # print(json.dumps(data['data']['req_info'], indent=4, ensure_ascii=False, sort_keys=True))
             reqinfo = data['data']['req_info']
             info = []
+            global Send_index
             for i in range(len(reqinfo)):
-                field_name = reqinfo[i]['field_name']
-                field_key = reqinfo[i]['field_key']
-                if field_name in UserInfo.keys():
+                field_name = Reg_Exp(reqinfo[i]['field_name'])#判断
+                field_key = reqinfo[i]['field_key']#填写
+                type_text = reqinfo[i]['type_text']#判断
+                if field_name in UserInfo.keys():#正则判断
                     field_value = UserInfo[field_name]
+                    if field_name == '学院' and type_text == '单项选择':#学院有单项选择题
+                        options = reqinfo[i]['new_options']
+                        for j in range(len(options)):#学院选项
+                            if options[j]['value'] == Reg_Exp(UserInfo[field_name]):#学院都是中文吧？
+                                new_field_value = options[j]['key']   
+                            else:
+                                print('这。。。如果学院都不匹配的话，手抢吧')
                 else:
                     field_value = UserInfo['未找到']
-                date = {"field_name": field_name, "field_value": field_value, "field_key": field_key, "ignore": 0}
+                    Send_index = 0
+                if type_text == '描述':#判断
+                    # field_name = reqinfo[i]['field_name']
+                    field_value = ""
+                    Send_index = 1
+                if type_text == '单张图片':#判断
+                    pic_url = upload_pic(eid)
+                    field_value = [pic_url]
+                    Send_index = 1
+                date = {"field_name": reqinfo[i]['field_name'], "field_value": field_value, "field_key": field_key, "ignore": 0}
+                if type_text == '单张图片':#需要单独改data构造
+                    date["origin_field_value"] = [pic_url]
+                if field_name == '学院' and type_text == '单项选择':#需要单独改data构造
+                    date["new_field_value"] = new_field_value
+                print(date)
                 # date = json.dumps(date)
                 info.append(date)
             # print(info)
@@ -169,8 +238,6 @@ def get_optioninfo(eid,token):
             print("\n获取infoti失败，重新获取")
             info = []
     return info
-
-
 
 def clicked_button(token,index,start,num):
     # print(token)
@@ -183,11 +250,9 @@ def clicked_button(token,index,start,num):
     url = "http://api-xcx-qunsou.weiyoubot.cn/xcx/enroll/v5/enroll"
     # print(ClassEid[index],'   ',token)
     # print(get_optioninfo(ClassEid[index],token))
-
     # input("\n讲座要求是否已经配置要求，如是请回车!")
     while 1:
         t = time.time()
-
         # 多次测试提前2秒开始抢没问题，当然你觉得不行，也可以提前1秒开始
         if t >= start-4:
             while(1):
@@ -197,13 +262,18 @@ def clicked_button(token,index,start,num):
                     data = {
                         "access_token": token,
                         "eid": ClassEid[index],
-                        "info": get_optioninfo(ClassEid[index],token),
+                        "info": get_optioninfo(ClassEid[index],token),#
                         "on_behalf": 0,
                         "items": [],
                         "referer": "",
                         "fee_type": ""
                     }
-                    ret = requests.post(url, headers=headers, data=json.dumps(data), verify=False, timeout=2).json()
+                    global Send_index
+                    if Send_index == 1:
+                        ret = requests.post(url, headers=headers, data=json.dumps(data), verify=False, timeout=2).json()
+                    elif Send_index == 0:
+                        print('存在未知报名参数，暂停提交')
+                        ret['msg'] = ''
                     # print(ret)
                     # print(ret['sta'])
                     if ret['msg'] == '':
@@ -220,12 +290,13 @@ def clicked_button(token,index,start,num):
                     elif ret['msg'] == '活动名额已满':
                         return
                     elif ret['msg'] == '活动期间，只允许提交1次':
-
+                        print(ret['msg'])
                         return
                     else:
                         print('线程', num , '（', ClassList[ClassEid[index]], '）:', ret)
                         # print(ret)
                         time.sleep(0.7)
+                        # break
                         continue
                 except requests.exceptions.RequestException as e:
                     # print(e)
@@ -233,17 +304,11 @@ def clicked_button(token,index,start,num):
                     print('线程',num,'（',ClassList[ClassEid[index]],'）:出错了')
                     time.sleep(0.5)
                     continue
+                    # break
             return
         time.sleep(1)
         print('\r线程',num,'（',ClassList[ClassEid[index]],'）开抢:还剩',int(start-t),'秒，当前时间:',datetime.now().strftime("%Y-%m-%d %H:%M:%S"),end='||||||||||')
         # print("当前时间：" + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
-
-
-
-
-
-
 
 # 抢单个活动
 # index = int(input("请选择抢购讲座编号(填编号):"))
@@ -252,7 +317,6 @@ def clicked_button(token,index,start,num):
 #
 # info =GetInfo(index) #获取报名要求信息
 # clicked_button(token,index ,info,start)
-
 
 class myThread(threading.Thread):
     def __init__(self,token, index, starttime, name):
@@ -273,8 +337,7 @@ class myThread(threading.Thread):
     def __del__(self):
         print (self.name,"线程结束！")
 
-
-
+# In[]
 if __name__ =='__main__':
     token = GetToken()
     UserInfo=ReadInfo()
